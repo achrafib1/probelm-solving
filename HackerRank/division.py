4a2c0ab21c976f51228f0f5f21192e58f2f347b4
achrafib1 <achibbuisness512@gmail.com>
2026-01-26 20:45:06 +0100

    Solve HackerRank problem
     - Add the solution for the Division problem

---
+++ HackerRank/division.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+
+    # The result of integer division, a//b
+    print(a // b)
+
+    # The result of integer division, a/b
+    print(a / b)

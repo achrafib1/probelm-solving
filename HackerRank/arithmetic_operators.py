88707aec2dcb767dd650e35525d39c289bdb3a06
achrafib1 <achibbuisness512@gmail.com>
2026-01-26 20:39:20 +0100

    Solve HackerRank problem
     - Solve the Arithmetic Operators problem

---
+++ HackerRank/arithmetic_operators.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+
+    # The sum of a and b
+    print(a + b)
+
+    # The difference of a and b
+    print(a - b)
+
+    # The product of a and b
+    print(a * b)

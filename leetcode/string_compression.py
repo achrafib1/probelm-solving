17269a9d7881730ac4be48ac47c24dd8f0a049a3
achrafib1 <achibbuisness512@gmail.com>
2026-03-13 17:17:52 +0100

    Solve Leetcode problem
     - Add solution for the string compression problem

--- leetcode/string_compression.py
+++ leetcode/string_compression.py
@@ -1,1 +1,31 @@
 # https://leetcode.com/problems/string-compression/description/
+
+
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+
+        ans = []
+        comp_s = ""
+        freq_dict = {}
+        curr_count = 1
+        for i in range(len(chars) - 1):
+            if chars[i] == chars[i + 1]:
+                curr_count += 1
+            else:
+                comp_s += chars[i] + str(curr_count) if curr_count > 1 else chars[i]
+                curr_count = 1
+
+        comp_s += (
+            chars[len(chars) - 1] + str(curr_count)
+            if curr_count > 1
+            else chars[len(chars) - 1]
+        )
+
+        for i in range(len(comp_s)):
+            chars[i] = list(comp_s)[i]
+
+        return len(chars[: len(comp_s)])

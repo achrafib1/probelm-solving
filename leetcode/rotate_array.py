ab15ca1a6a9fc144726bc57a89bf74dedcc50a92
achrafib1 <achibbuisness512@gmail.com>
2026-03-25 14:10:27 +0100

    Solve Leetcode problem
     - Add solution for the rotate array problem

---
+++ leetcode/rotate_array.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/rotate-array/description/
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nums[:] = (
+            nums[len(nums) - (k % len(nums)) :] + nums[: len(nums) - (k % len(nums))]
+        )
+        return

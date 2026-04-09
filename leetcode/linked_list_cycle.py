74d463f53314e73320561c4f01c04118c99fec53
achrafib1 <achibbuisness512@gmail.com>
2026-04-09 21:31:24 +0100

    Solve Leetcode problem
     - Add solution for linked list cycle problem

--- leetcode/linked_list_cycle.py
+++ leetcode/linked_list_cycle.py
@@ -1,1 +1,29 @@
 # https://leetcode.com/problems/linked-list-cycle/description/
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+        fast = head
+        slow = head
+
+        ans = False
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                ans = True
+                break
+
+        return ans
